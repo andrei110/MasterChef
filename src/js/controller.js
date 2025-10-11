8a8ad152fee49dad61e63794dda29ce5606ad6fe
andrei110 <andrei_vaida98@yahoo.com>
2025-10-12 00:29:17 +0300

    feature: searchBar - searching for queries functionality was added

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,18 +1,32 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
 
 const controlRecipe = async function () {
   try {
     const id = window.location.hash.slice(1);
     console.log(id);
+
+    // Load recipe data
     await model.loadRecipe(id);
+
+    // Render recipe data
     recipeView.render(model.state.recipe);
   } catch (err) {
     console.error(err);
   }
 };
+
+// Search Results
+const controlSearchResults = async function () {
+  const query = searchView.getQuery();
+  console.log(query);
+};
+
+// App init
 const init = function () {
   recipeView.addHandlerRender(controlRecipe);
+  searchView.addHandlerSearch(controlSearchResults);
 };
 
 init();
--- src/js/model.js
+++ src/js/model.js
@@ -25,9 +25,11 @@ export const loadRecipe = async function (id) {
       instructions: data.analyzedInstructions,
       ingredients: data.nutrition.ingredients,
       nutrition: {
-        protein: data.nutrition.caloricBreakdown.percentProtein,
-        fats: data.nutrition.caloricBreakdown.percentFat,
-        carbs: data.nutrition.caloricBreakdown.percentCarbs,
+        protein: data.nutrition.nutrients.find(n => n.name === 'Protein')
+          .amount,
+        fats: data.nutrition.nutrients.find(n => n.name === 'Fat').amount,
+        carbs: data.nutrition.nutrients.find(n => n.name === 'Carbohydrates')
+          .amount,
         calories: data.nutrition.nutrients.find(n => n.name === 'Calories')
           .amount,
       },
